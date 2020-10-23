5f6b7ae3f95af589d9619331198265ce54264329
jshafto <jpshafto@gmail.com>
2020-10-23 18:57:51 -0400

    added python testing page

---
+++ client/src/components/PythonTerminal.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PythonTerminal (props) {
+    return (
+        <>
+            <h1>Python stuff: </h1>
+
+        </>
+        );
+}
+
+export default PythonTerminal;
