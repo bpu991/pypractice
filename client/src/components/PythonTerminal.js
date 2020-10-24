04aec60e9a79b0814e972b0542e518d75845ab01
jon <jon@jons-MacBook-Pro.local>
2020-10-24 13:36:43 -0500

    new branch

--- client/src/components/PythonTerminal.js
+++ client/src/components/PythonTerminal.js
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, createElement } from "react";
 
-function PythonTerminal(props) {
+export default function PythonTerminal() {
   useEffect(() => {
     const script1 = document.createElement("script");
     script1.src = "https://pyodide-cdn2.iodide.io/v0.15.0/full/";
@@ -11,16 +11,28 @@ function PythonTerminal(props) {
     document.body.appendChild(script1);
     document.body.appendChild(script2);
 
-    // return () => {
-    //   document.body.removeChild(script);
-    // };
+    return () => {
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
+    };
   }, []);
+
+  const handleClick = () => {
+    let pythonCode = `
+      return "say hello"
+    `;
+    const script = document.createElement("script");
+    script.innerText = `console.log(pyodide.runPython(${pythonCode}));`;
+    document.body.appendChild(script);
+    document.body.removeChild(script);
+  };
 
   return (
     <>
       <h1>Python stuff: </h1>
+      <button type='button' onClick={handleClick}>
+        Click
+      </button>
     </>
   );
 }
-
-export default PythonTerminal;
