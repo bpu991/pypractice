243bd666802d80944207f3b4365196b306aec069
jon <jon@jons-MacBook-Pro.local>
2020-10-24 19:14:49 -0500

    functional front end python editor and interpreter

---
+++ client/public/js/python-repl.js
@@ -0,0 +1,25 @@
+//create host element
+const aceEditor = document.createElement("div");
+//append to body
+document.body.append(aceEditor);
+//initialize code editor in element
+const acer = window.ace.edit(aceEditor, {
+  mode: "ace/mode/python",
+  selectionStyle: "text",
+});
+//configure ace code editor
+acer.setOptions({
+  enableBasicAutocompletion: true,
+  enableLiveAutocompletion: true,
+  maxLines: Infinity,
+  fontSize: "100%",
+  showPrintMargin: true,
+  minLines: 40,
+  maxLines: Infinity,
+  fontSize: "100%",
+  mode: "ace/mode/python",
+  selectionStyle: "text",
+  autoScrollEditorIntoView: true,
+  animatedScroll: true,
+});
+acer.setTheme("ace/theme/tomorrow_night_blue");
--- client/src/components/PythonTerminal.js
+++ client/src/components/PythonTerminal.js
@@ -1,38 +1,81 @@
-import React, { useState, useEffect, createElement } from "react";
+import React, { useEffect } from "react";
+// @material-ui/core
+// import { makeStyles } from "@material-ui/core/styles";
+
+import "./PythonTerminal.css";
 
 export default function PythonTerminal() {
   useEffect(() => {
-    const script1 = document.createElement("script");
-    script1.src = "https://pyodide-cdn2.iodide.io/v0.15.0/full/";
-
-    const script2 = document.createElement("script");
-    script2.src = "https://pyodide-cdn2.iodide.io/v0.15.0/full/pyodide.js";
-
-    document.body.appendChild(script1);
-    document.body.appendChild(script2);
-
-    return () => {
-      document.body.removeChild(script1);
-      document.body.removeChild(script2);
-    };
-  }, []);
-
-  const handleClick = () => {
-    let pythonCode = `
-      return "say hello"
-    `;
     const script = document.createElement("script");
-    script.innerText = `console.log(pyodide.runPython(${pythonCode}));`;
-    document.body.appendChild(script);
-    document.body.removeChild(script);
-  };
+    script.src = "/js/python-repl.js";
+    document.body.append(script);
+  });
+
+  function handleClick() {
+    const script2 = document.createElement("script");
+    script2.innerText = `
+var code = acer.getValue();
+console.log(code); 
+var py = pyodide.runPython;
+console.log(py("2+2"));
+
+var a = py("'Hello Py'");
+
+var display = document.querySelector("#display");
+display.innerHTML = a;
+
+var b = py(code);
+display.innerHTML = b;
+    
+    `;
+    document.body.append(script2);
+
+    return () => document.body.remove(script2);
+  }
 
   return (
     <>
       <h1>Python stuff: </h1>
-      <button type='button' onClick={handleClick}>
-        Click
-      </button>
+      <h2>Recursion-1 --</h2>
+      <p>
+        <strong>StarBit</strong> **This question is modified from parenBit on
+        CodingBat to starBit.
+      </p>
+      <p>
+        Given a string that contains a dash and a star, compute recursively a
+        new string made of only of the dash and star and their contents, so
+        "xyz-abc*123" yields "-abc*".
+      </p>
+      <p style={{ backgroundColor: "lightgray", fontWeight: "bold" }}>
+        Examples:
+        <br />
+        starBit('xyz,-abc*123') → -abc* starBit('x,-hello*') → -hello*
+        starBit(',-xy*1') → -xy*
+      </p>
+      <p>Result:</p>
+      <div id='display'></div>
+      <button onClick={handleClick}>Click</button>
     </>
   );
 }
+
+// def hello():
+//     s=''
+//     a=['h','i']
+//     for char in a:
+//         s += char
+//     return s
+// b = hello()
+// b
+
+// def star_bit(string):
+//     if string[0] == "-" and string[-1] == "*":
+//         return string
+//     if not string[0] == "-":
+//         string = string[1:]
+//     if not string[-1] == "*":
+//         string = string[:-1]
+//     return star_bit(string)
+
+// result = star_bit(xyz,-abc*123)
+// result
