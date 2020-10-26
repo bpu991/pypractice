4b8af82bb6e3608cafa203a92b766aedab64632a
Andrew <travers.drew@gmail.com>
2020-10-26 19:59:01 -0400

    Created Test Module

--- database.py
+++ database.py
@@ -1,24 +1,16 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-from starter_app import app, db
-from starter_app.models import User
+from app import app, db
+from app.models import User
 
 with app.app_context():
   db.drop_all()
   db.create_all()
 
-  ian = User(username = 'Ian', email = 'ian@aa.io')
-  javier = User(username = 'Javier', email = 'javier@aa.io')
-  dean = User(username = 'Dean', email = 'dean@aa.io')
-  angela = User(username = 'Angela', email = 'angela@aa.io')
-  soonmi = User(username = 'Soon-Mi', email = 'soonmi@aa.io')
-  alissa = User(username = 'Alissa', email = 'alissa@aa.io')
+  soonmi = User(username = 'Soon-Mi', email = 'soonmi@aa.io', password = 'password')
+  alissa = User(username = 'Alissa', email = 'alissa@aa.io', password = 'password')
 
-  db.session.add(ian)
-  db.session.add(javier)
-  db.session.add(dean)
-  db.session.add(angela)
   db.session.add(soonmi)
   db.session.add(alissa)
 
