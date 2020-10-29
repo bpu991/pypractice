e69a9716f5a7ab537424fc5ef197123c6a9af9b2
jon <jon@jons-MacBook-Pro.local>
2020-10-29 10:33:02 -0500

    result table + refactoring

--- client/src/utils.js
+++ client/src/utils.js
@@ -1,35 +1,34 @@
+export default class pyTester {
+  constructor(problem, py) {
+    this.problemId = problem.id;
+    this.sol = problem.solution;
+    this.args = problem.tests;
+    this.attempt = "";
 
+    this.py = py;
+  }
 
-export class pyTester {
-    constructor(problem) {
-        this.problemId = problem.id;
-        this.sol = problem.solution;
-        this.args = problem.tests;
-        this.attempt = '';
-    }
+  setAndRun(attempt) {
+    //get user's attempted solution from ACE terminal
+    this.attempt = attempt;
+    this.runTests();
+  }
 
-    setAndRun(attempt) {
-        //get user's attempted solution from ACE terminal
-        this.attempt = attempt;
-        this.runTests();
-    }
-
-    runTests() {
-        let results = []
-        for(let i =0; i < this.args.length; i++) {
-            const attempt = pyodide.runPython(`
+  runTests() {
+    let results = [];
+    for (let i = 0; i < this.args.length; i++) {
+      const attempt = this.py(`
 ${this.attempt}
 result = ${this.args[i].call}
 result
-            `)
-            results.push({
-                call: this.args[i].call,
-                expected: this.args[i].expected,
-                result: attempt,
-                pass: attempt == this.args[i].expected,
-            })
-        }
-    return results
+            `);
+      results.push({
+        call: this.args[i].call,
+        expected: this.args[i].expected,
+        result: attempt,
+        pass: attempt == this.args[i].expected,
+      });
     }
-
+    return results;
+  }
 }
